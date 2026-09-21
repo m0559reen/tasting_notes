d673a5800d5a111f8cdb26a0a0d7716e6fc95cca
m0559reen <mossgreen0215@gmail.com>
2026-09-21 13:14:04 +0900

    modify v2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker: アプリ本体をキャッシュしてオフラインでも動くようにする。
 // index.html などを更新して再公開したときは、下の VERSION を上げてください。
-const VERSION = 'v1';
+const VERSION = 'v2';
 const CACHE = 'tasting-notes-' + VERSION;
 const ASSETS = [
   './',
